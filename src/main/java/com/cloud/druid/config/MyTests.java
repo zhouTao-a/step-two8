fe45a68e18e29e99fc486233c36079b84f273acc
zhout <987171620@qq.com>
2023-02-23 09:08:28 +0800

    代码修改

---
+++ src/main/java/com/cloud/druid/config/MyTests.java
@@ -0,0 +1,1 @@
+package com.cloud.druid.config;import com.alibaba.druid.filter.config.ConfigTools;/** * 调用druid生成秘钥 */class MyTests {    public static void main(String[] args) throws Exception {        // 需要加密的明文命名        String password = "qaz123WSX"; // 【注意：这里要改为你自己的密码】        // 调用 druid 生成私钥、公钥、密文        ConfigTools.main(new String[]{password});    }}
